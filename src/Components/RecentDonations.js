851b4b4d8169544e1b0a5e61d9ddf5864a05cf3f
jeremytjimenez <jeremytjimenez@gmail.com>
2023-04-19 17:39:04 -0400

    all tests completed

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,23 @@
-export default function RecentDonations() {
-  return null;
+function IndividualDonation(props) {
+  return (
+    <li><span>{props.data.name} donated ${props.data.amount}</span>{props.data.caption}</li>
+  )
+}
+
+export default function RecentDonations(props) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {props.donationData.map((donation) => {
+            return (
+              <IndividualDonation 
+              key={donation.id}
+              data={donation}
+              />
+            )
+        })}
+      </ul>
+    </section>
+  )
 }
