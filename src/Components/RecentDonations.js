af51700b183ce030aafd23b6e603a6f55eaaa91c
lgxxhi <luisgsilva95@gmail.com>
2023-04-19 19:51:14 -0400

    Completed Lab

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,16 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({ amount, caption, name }) {
+  return (
+    <div className="recent-donations">
+      <div className="recent-donations-data">
+        <ul>
+          <li>
+            <span>
+              {name} donated ${amount}
+            </span>
+            {caption}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
