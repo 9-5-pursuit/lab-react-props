951742e8b3d00e351ad18734c82867560847fe44
Michael Reyes <michaelreyes@Michaels-Air.fios-router.home>
2023-04-20 09:27:55 -0400

    it's beautiful

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,20 @@
-export default function Progress() {
-  return null;
+import React from "react";
+
+function Progress(props) {
+  const { donations, targetAmount } = props;
+  const total = donations.reduce(
+    (total, donation) => total + donation.amount,
+    0
+  );
+
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${total}</span> of{" "}
+        <span className="secondary">${targetAmount}</span>
+      </h2>
+    </section>
+  );
 }
+
+export default Progress;
