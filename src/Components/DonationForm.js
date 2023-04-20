0d9e26543baf35a82069048bac647e20c04589f4
Chantal <chantalgomez@pursuit.org>
2023-04-19 23:55:46 -0400

    DOne

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,3 +1,27 @@
-export default function DonationForm() {
-  return null;
+
+export default function DonationForm(props) {
+
+  const {arrayLenghtAndOne} = props
+
+  return (
+    
+    
+    <section className="donation">
+      <h3>You could be donation <span class="secondary">#{arrayLenghtAndOne}!</span></h3>
+
+  
+  <form>
+    <label htmlFor="name">Name</label>
+    <input id="name" name="name" type="text" placeholder="Your Name..."></input>
+
+    <label htmlFor="caption">Caption</label>
+    <input id="caption" name="caption" type="text" placeholder="Add a brief message..."></input>
+
+    <label htmlFor="amount">Amount</label>
+    <input id="amount" name="amount" type="number" placeholder="0"></input>
+    <button>Donate!</button>s
+  </form>
+</section>
+    
+  );
 }
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,13 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  // const {amount} = props
+  // console.log(amount);
+
+  return (
+    <section className="progress">
+     <h2>
+     Raised <span className="secondary">$${props.amount.reduce((prev, next) => { return prev + next.props.amount }, 0)}</span> of
+     <span className="secondary">$1000</span>
+     </h2>
+</section>
+  );
 }
