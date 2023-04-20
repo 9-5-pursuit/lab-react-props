fef524adc9cc4a9823c624389c7571b1b65bf164
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-04-20 02:25:04 -0400

    All cypress tests passed

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,10 @@
-export default function Progress() {
-  return null;
+export default function Progress({ collected, targetAmount }) {
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${collected} </span>of
+        <span className="secondary"> ${targetAmount}</span>
+      </h2>
+    </section>
+  );
 }
