dcf07b72edd2c01139313e42ac4db4dba1f82f12
Irfan Hanjra <ihanjra@gmail.com>
2023-04-20 04:33:06 -0400

    Initial commit

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,20 @@
-export default function RecentDonations() {
-  return null;
+import React from "react";
+
+function RecentDonations(props) {
+  const { donations } = props;
+  return (
+  <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map((donation) => (
+          <li key={donation.id}>
+            <span>{donation.name} donated ${donation.amount}</span>
+            {donation.caption}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
+
+export default RecentDonations;
