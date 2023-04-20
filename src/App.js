c1a5511df2d64e8121be5d4c24c166acc674c72d
rboyce212 <richboyce@pursuit.org>
2023-04-20 09:58:41 -0400

    ran out of time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
 import "./App.css";
+import DonationForm from "./Components/DonationForm";
+import Progress from "./Components/Progress";
+import RecentDonations from "./Components/RecentDonations";
 
 const targetAmount = 1000;
 const donations = [
@@ -8,32 +11,32 @@ const donations = [
     amount: 250,
     caption: "You really need this. Really.",
     id: 1,
-    name: "Jo",
+    name: "Jo"
   },
   {
     amount: 30,
     caption: "Here, take a break from work!",
     id: 2,
-    name: "Rami",
+    name: "Rami"
   },
   {
     amount: 20,
     caption: "LOL! You are too funny. Happy to do this for you. :)",
     id: 3,
-    name: "Michelle",
+    name: "Michelle"
   },
   {
     amount: 5,
     caption: "Have fun!",
     id: 4,
-    name: "Malinda",
+    name: "Malinda"
   },
   {
     amount: 30,
     caption: "Come visit me in Miami!",
     id: 5,
-    name: "Sam",
-  },
+    name: "Sam"
+  }
 ];
 
 function App() {
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,19 @@
-export default function RecentDonations() {
-  return null;
-}
+const RecentDonations = (props) => {
+  const { amount, caption, id, name } = props;
+  const listDonations = donations.map((donation) => (
+    <li key={donation.id}>{donation.content}</li>
+  ));
+  return (
+    <ul className="recentDonations">
+      <h2>Recent Donations</h2>
+      <li>
+        <span>
+          {name} donated ${amount}
+        </span>
+        {caption}
+      </li>
+    </ul>
+  );
+};
+
+export default RecentDonations;
