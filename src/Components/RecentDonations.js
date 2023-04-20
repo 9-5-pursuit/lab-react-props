c647fc62e1c5d7344a4d66e80770abb026a91071
Jennifer <jenniferpeterson@pursuit.org>
2023-04-20 08:25:18 -0400

    react props lab

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,23 @@
-export default function RecentDonations() {
-  return null;
-}
+const RecentDonations = (props) => {
+  const { donations } = props;
+  // console.log(props);
+  let info = donations.map((donation) => {
+    return (
+      <li>
+        <span>
+          {donation.name} donated ${donation.amount}
+        </span>
+        {donation.caption}
+      </li>
+    );
+  });
+  // console.log(donations);
+
+  return (
+    <span>
+      <h2>Recent donations</h2>
+      <ul>{info}</ul>
+    </span>
+  );
+};
+export default RecentDonations;
