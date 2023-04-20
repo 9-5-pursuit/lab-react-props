c1a0d8f13b8fed6cef1b7d349170403058a45e5d
zazimov <zalmanazimov@pursuit.org>
2023-04-20 00:15:02 -0400

    fully rendered page

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,17 @@
-export default function Progress() {
-  return null;
-}
+const Progress = (props) => {
+  const { donations, targetAmount } = props;
+  let sum = 0;
+  donations.map((ele) => {
+    sum += ele.amount;
+  });
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${sum}</span> of
+        <span className="secondary"> ${targetAmount}</span>
+      </h2>
+    </section>
+  );
+};
+
+export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,22 @@
-export default function RecentDonations() {
-  return null;
-}
+import Donations from "./Donations";
+
+const RecentDonations = (props) => {
+  const { donations } = props;
+  let output = donations.map((donation) => {
+    return (
+      <Donations
+        amount={donation.amount}
+        caption={donation.caption}
+        name={donation.name}
+      />
+    );
+  });
+  return (
+    <span>
+      <h2>Recent Donations</h2>
+      <ul>{output}</ul>
+    </span>
+  );
+};
+
+export default RecentDonations;
