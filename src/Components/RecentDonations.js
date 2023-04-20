421d857a145b2d7d82fe5e72146c423c75dacd36
Onyx McQueen <onyxmcqueen88@gmail.com>
2023-04-20 13:21:02 -0400

    Lab Completed

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,18 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  const { donations } = props;
+
+let placeholder = donations.map(donation => {
+  return (
+    <li key={donation.id}><span>{donation.name} donated ${donation.amount}</span>{donation.caption}</li>
+    )
+})
+
+  return (
+  <section>
+    <h2>Recent Donations</h2>
+    <ul>
+    {placeholder}
+    </ul>
+  </section>
+  )
 }
