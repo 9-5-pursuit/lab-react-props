1de8b754d69a15eabec8fe74855dd766e9af621f
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-04-20 09:53:23 -0400

    Lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,25 @@
-export default function Progress() {
-  return null;
+export default function Progress({ donations }) {
+  let paid = 0
+  for (let i = 0; i < donations.length; i++) {
+    const element = donations[i];
+    paid += element.amount;
+  }
+  
+
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${paid}</span> of
+        <span className="secondary">$1000</span>
+      </h2>
+    </section>
+  );
 }
+/*
+<section className="progress">
+  <h2>
+    Raised <span className="secondary">$0</span> of
+    <span className="secondary">$1000</span>
+  </h2>
+</section>
+*/
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,37 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({ donations }) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map((item) => {
+          return (
+            <li>
+              <span>
+                {item.name} donated ${item.amount}
+              </span>
+              {item.caption}.
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
+/*
+<section>
+  <h2>Recent Donations</h2>
+  <ul>
+    <li><span>Jo donated $25</span>You really need this. Really.</li>
+    <li><span>Rami donated $10</span>Here, take a break from work!</li>
+    <!-- etc... -->
+  </ul>
+</section>
+
+
+{
+    amount: 250,
+    caption: "You really need this. Really.",
+    id: 1,
+    name: "Jo",
+  },
+*/
