26788befec4efeb1e6ad12342aafcc92aaf46412
Jibon P <jibonp@Jibons-MacBook-Air.local>
2023-04-20 11:24:42 -0400

    done

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,13 @@
-export default function Progress() {
-  return null;
+export default function Progress({ targetAmount, donations }) {
+  const Money = donations.reduce((total, { amount }) => {
+    return (total += amount);
+  }, 0);
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${Money}</span> of
+        <span className="secondary"> ${targetAmount}</span>
+      </h2>
+    </section>
+  );
 }
