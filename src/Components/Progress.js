18592a97de91889963c2ef9f9abf336ff6c49283
Blui322 <luistejada@pursuit.com>
2023-04-26 19:49:12 -0400

    done

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,15 @@
-export default function Progress() {
-  return null;
-}
+const Progress = ({donations}) => {
+  let sum = 0;
+  donations.map((element) => {
+    sum += element.amount;
+  });
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${sum}</span> of 
+        <span className="secondary"> $1000</span>
+      </h2>
+    </section>
+  );
+};
+export default Progress
